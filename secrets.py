749e1ca29f9afe91df5ba1ee189e5aaddf6578d1
cschroedt <z08732-0002@gmx.de>
2026-07-14 12:55:28 +0000

    Add files via upload

---
+++ secrets.py
@@ -0,0 +1,9 @@
+secrets = {
+'ssid': 'R21W',
+'wlpassword': 'Rainlesberg12',
+'webhooks_key': 'WEBHOOKS_API_KEY',
+'username': 'z087320002@gmail.com'
+'mpassword': 'tqrthrlfpjimflxy'
+'sendTo': 'z08732-0002@gmx.de'
+'openweather_api_key': 'af4381d08ad2729677cfdab453f82b38'
+}
