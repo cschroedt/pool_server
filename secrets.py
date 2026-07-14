2fd77d55fe087c7334e3a91ba8c1ed12da7cda88
cschroedt <z08732-0002@gmx.de>
2026-07-14 12:45:34 +0000

    Delete secrets.py

--- secrets.py
+++
@@ -1,8 +0,0 @@
-secrets = {
-'ssid': 'R21W',
-'wlpassword': 'Rainlesberg12',
-'webhooks_key': 'WEBHOOKS_API_KEY',
-'username': 'z087320002@gmail.com'
-'mpassword': 'tqrthrlfpjimflxy'
-'sendTo': 'z08732-0002@gmx.de'
-}
