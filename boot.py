0f91017a0a3d826138a1017cc7faab9c8384efea
cschroedt <z08732-0002@gmx.de>
2026-07-14 13:15:08 +0000

    Refactor boot.py for improved Wi-Fi connection logic

--- boot.py
+++ boot.py
@@ -5,20 +5,21 @@ from ota_updater import OTAUpdater
 
 # --- Wi-Fi Configuration ---
 # You've selected these constants in your MainActivity.java, so we'll use them here.
-WIFI_SSID = secrets['ssid'] #"R21W"  # Replace with your actual Wi-Fi SSID
-WIFI_PASSWORD = secrets['password'] #"Rainlesberg12" # Replace with your actual Wi-Fi password
+WIFI_SSID = secrets['ssid'] # Replace with your actual Wi-Fi SSID
+WIFI_PASSWORD = secrets['password'] # Replace with your actual Wi-Fi password
 
 # --- OTA Configuration ---
 # This is the raw content URL for your GitHub repository.
 # IMPORTANT: Replace "your-username/your-repo" with your actual username and repository name.
 FIRMWARE_URL = "https://raw.githubusercontent.com/cschroedt/pool_server/main"
 # List of files to update from the repository
-FILES_TO_UPDATE = ['main1.py', 'version.txt', 'do_connect.py', 'boot.py', 'secrets.py'] # Add any other files you need
+FILES_TO_UPDATE = ['boot.py', 'main1.py', 'version.txt', 'do_connect.py', 'secrets.py'] # Add any other files you need
 
 # --- Connect to Wi-Fi ---
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
-wlan.connect(WIFI_SSID, WIFI_PASSWORD)
+#wlan.connect(WIFI_SSID, WIFI_PASSWORD)
+wlan.connect('R21W', 'Rainlesberg12')
 
 # Wait for connection
 max_wait = 10
@@ -41,16 +42,6 @@ else:
 datei=open("version.txt",'r')
 msg=datei.read()
 datei.close
-import umail
-try:
-    smtp = umail.SMTP('smtp.gmail.com', 587, username='z087320002@gmail.com', password='tqrthrlfpjimflxy')
-    smtp.to('z08732-0002@gmx.de')
-    smtp.write("Subject: Meldung vom Poolserver\n\n")
-    smtp.write("Neustart des Programms: Version "+msg)
-    smtp.send()
-    smtp.quit()
-except:
-    print("Something wrong with eMail, not sent")
 wlan.disconnect()
 wlan.active(False)
 wlan=None
